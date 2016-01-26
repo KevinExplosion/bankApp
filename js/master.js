80769d7ad77f1357ee633ded569c31541080810c
Kevin Mattison and Tim Durward <mattisonii@yahoo.com>
2016-01-26 13:11:36 -0800

    add jquery to store values into object: bankAccount

--- js/master.js
+++ js/master.js
@@ -12,5 +12,9 @@ function BankAccount(name, deposit){
 $(document).ready(function() {
   $("#createAccount").click(function(){
     //click "CREATE ACCOUNT" ; resume after lunch
+    var bank_name = $("#bankName").val();
+    var initial_deposit = $("#bankInitialDeposit").val();
+
+    var bankAccount = new BankAccount(bank_name, initial_deposit);
   });
 });
