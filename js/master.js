112e5dbde71c2461d3c2d05981df242bcb106edc
Kevin Mattison and Tim Durward <timothy.a.durward@gmail.com>
2016-01-26 14:22:54 -0800

    include functionality to withdraw money from account balance

--- js/master.js
+++ js/master.js
@@ -20,14 +20,26 @@ $(document).ready(function() {
     var bankAccount = new BankAccount(bank_name, initial_deposit);
     $(".bankInfo").hide();
     $(".accountInfo").show();
+    $("#accountName").text(bank_name);
     $("#accountBalanceForm").val(initial_deposit);
+
+
     $("#submitAmount").click(function(){
+
+      //prototype adding deposited money
       BankAccount.prototype.depoMoney = function(){
-      return this.initialDeposit + depositDollars;}
+        return this.initialDeposit + depositDollars;
+      }
+      //prototype subtracting withdrawn money
+      BankAccount.prototype.withdrawMoney = function() {
+        return this.initialDeposit - withdrawDollars;
+      }
+
       var depositDollars = parseInt($("#bankDeposit").val());
+
+
       var withdrawDollars= parseInt($("#bankWithdraw").val());
       var accountBalanceTextform = parseInt($("#accountBalanceForm").val(bankAccount.depoMoney()));
-        // parseInt$("#accountBalanceForm").val();
     });
   });
 });
