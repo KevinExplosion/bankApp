781e4b1823c9a9c3d40d9adc11e0a9f98f9f89a1
Kevin Mattison and Tim Durward <mattisonii@yahoo.com>
2016-01-26 11:22:16 -0800

    add first spec

--- spec/specs.js
+++ spec/specs.js
@@ -6,3 +6,12 @@
 //     expect(functionname(parameter)).to.equal('placeholder');
 //   });
 // });
+
+describe("bankAccount", function(){
+  it("will return true if bank account object parameters are met", function() {
+    var newBankAccount = new bankAccount("Bob", "checking", 500)
+      excpet(bankAccount.name).to.equal("Bob");
+      excpet(bankAccount.type).to.equal("checking");
+      expect(bankAccount.initialDeposit).to.equal(500);
+  });
+});
